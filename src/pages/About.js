23455bae46d08ad27fcc91ccdfecf5f2076e9ed7
popovic-nedeljko <nftnedja@gmail.com>
2022-12-21 15:58:58 +0100

    About and Error page setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
+        Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan
+        marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear
+        claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi
+        bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie
+        gummies.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
