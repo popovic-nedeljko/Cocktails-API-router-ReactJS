81024b8fbfd0ca548836c840ce9ed3a03abd00e7
popovic-nedeljko <nftnedja@gmail.com>
2022-12-22 21:42:56 +0100

    Search form funcionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,11 +3,34 @@ import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = function () {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+  };
 
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search you favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
